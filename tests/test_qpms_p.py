a4b160dc41ed65cc7e2c60e740c5fee02d5c92a7
Marek Nečada <marek.necada@aalto.fi>
2016-07-22 15:35:41 +0300

    Skeleton of test test set
    Former-commit-id: 924ef7d968f027768b13e983d8bcac823538f738

---
+++ tests/test_qpms_p.py
@@ -0,0 +1,29 @@
+import unittest
+import qpms
+import numpy as np
+
+# Some constants go here.
+maxx = 20 # The maximum argument of the Bessel's functions, i.e. maximum wave number times the distance
+lMax = 25 # To which order we decompose the waves
+
+
+class PlaneWaveDecompositionTests(unittest.TestCase):
+
+    def testRandomInc(self):
+        pass
+
+    def testCornerCases(self):
+        pass
+
+
+class SphericalWaveTranslationTests(unittest.TestCase):
+
+    def sometest(self):
+        pass
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
+
