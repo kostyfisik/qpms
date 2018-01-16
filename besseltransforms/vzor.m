594075e83745b02bac5987452e73b790cb0034e5
Marek Nečada <marek@necada.org>
2018-01-16 20:21:29 +0000

    Vzor integrálů byl blbě
    Former-commit-id: bea91548fa572125a6aa6dce99c68f7ed4898b1c

--- besseltransforms/vzor.m
+++ besseltransforms/vzor.m
@@ -1,6 +1,6 @@
 $Assumptions = k >= 0 && k0 >= 0 && c >= 0 && n >= 0 ;
 Refine[Integrate[(1 - Exp[-c x])^\[Kappa] (k0 x)^(-q) Exp[
-    I k0 x] x BesselJ[n, k x] Exp[-c x], {x, 
+    I k0 x] x BesselJ[n, k x], {x, 
        0, \[Infinity]}], {\[Kappa] == kk, q == qq, n == nn}]
 Series[%, {k, \[Infinity], 10}]
 Quit[ ]
