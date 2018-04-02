c2ee695633dc763231820fcb9b97e6642a77ed35
Marek Nečada <marek@necada.org>
2018-04-02 20:49:27 +0300

    Fix kahansum init
    Former-commit-id: 306596052fa88952fcb182f69d79315fa9c24020

--- qpms/kahansum.h
+++ qpms/kahansum.h
@@ -1,9 +1,9 @@
 #ifndef KAHANSUM_H
 #define KAHANSUM_H
 
-static inline void kahaninit(double *sum, double *compensation) {
-	sum = 0;
-	compensation = 0;
+static inline void kahaninit(double * const sum, double * const compensation) {
+	*sum = 0;
+	*compensation = 0;
 }
 
 static inline void kahanadd(double *sum, double *compensation, double input) {
@@ -14,9 +14,9 @@ static inline void kahanadd(double *sum, double *compensation, double input) {
 }
 
 
-static inline void ckahaninit(complex double *sum, complex double *compensation) {
-	sum = 0;
-	compensation = 0;
+static inline void ckahaninit(complex double * const sum, complex double * const compensation) {
+	*sum = 0;
+	*compensation = 0;
 }
 
 static inline void ckahanadd(complex double *sum, complex double *compensation, complex double input) {
