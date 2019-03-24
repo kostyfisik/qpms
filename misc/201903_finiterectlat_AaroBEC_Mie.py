4f4d340f0e6970272a68a39ddd7128f0dc2d7eda
Marek Nečada <marek@necada.org>
2019-03-24 23:58:32 +0000

    Spherical particle version of the finite rectangular lattice script.
    Former-commit-id: f23cb1a92cec73f38cd0d79c90346797e4c16b67

---
+++ misc/201903_finiterectlat_AaroBEC_Mie.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+from qpms import Particle, CTMatrix, BaseSpec, FinitePointGroup, ScatteringSystem, TMatrixInterpolator, eV, hbar, c
+from qpms.symmetries import point_group_info
+import numpy as np
+import os
+import sys
+nm = 1e-9
+
+cyr_part_height = 50*nm
+cyr_part_radius = 50*nm
+cyr_part_volume = cyr_part_height * np.pi * cyr_part.radius**2
+eqv_sph_radius = (3/4/np.pi*cyr_part_volume)**(1/3)
+
+sym = FinitePointGroup(point_group_info['D2h'])
+bspec = BaseSpec(lMax = 2)
+#tmfile = '/m/phys/project/qd/Marek/tmatrix-experiments/Cylinder/AaroBEC/cylinder_50nm_lMax4_cleaned.TMatrix'
+materialfile = '/home/necadam1/wrkdir/repo/refractiveindex.info-database/database/data/main/Au/Johnson.yml'
+
+#outputdatadir = '/home/necadam1/wrkdir/AaroBECfinite_new'
+outputdatadir = '/u/46/necadam1/unix/project/AaroBECfinite_sph'
+os.makedirs(outputdatadir, exist_ok = True)
+mi = MaterialInterpolator(materialfile)
+#interp = TMatrixInterpolator(tmfile, bspec, symmetrise = sym, atol = 1e-8)
+# There is only one t-matrix in the system for each frequency. We initialize the matrix with the lowest frequency data.
+# Later, we can replace it using the tmatrix[...] = interp(freq) and s.update_tmatrices NOT YET; TODO
+
+omega = float(sys.argv[3]) * eV/hbar
+sv_threshold = float(sys.argv[4])
+
+# Now place the particles and set background index.
+px = 571*nm; py = 621*nm
+n = 1.52
+Nx = int(sys.argv[1])
+Ny = int(sys.argv[2])
+
+orig_x = (np.arange(Nx/2) + (0 if (Nx % 2) else .5)) * px
+orig_y = (np.arange(Ny/2) + (0 if (Ny % 2) else .5)) * py
+
+orig_xy = np.stack(np.meshgrid(orig_x, orig_y), axis = -1)
+
+#tmatrix = interp(omega)
+tmatrix = CTMatrix.spherical_perm(bspec, eqv_sph_radius, omega, mi(omega), n**2)
+particles = [Particle(orig_xy[i], tmatrix) for i in np.ndindex(orig_xy.shape[:-1])]
+
+
+ss = ScatteringSystem(particles, sym)
+
+
+k = n * omega / c
+
+
+for iri in range(ss.nirreps):
+    mm_iri = ss.modeproblem_matrix_packed(k, iri)
+    U, S, Vh = np.linalg.svd(mm_iri)
+    print(iri, ss.irrep_names[iri], S[-1])
+    starti = max(0,len(S) - np.searchsorted(S[::-1], sv_threshold, side='left')-1)
+    np.savez(os.path.join(outputdatadir, 'Nx%d_Ny%d_%geV_ir%d.npz'%(Nx, Ny, omega/eV*hbar, iri)),
+        S=S[starti:], omega=omega, Vh = Vh[starti:], iri=iri, Nx = Nx, Ny= Ny )
+    # Don't forget to conjugate Vh before transforming it to the full vector!
+
