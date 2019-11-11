21a1313a5556950bb337d57a499c0b2030bec11d
Marek Nečada <marek@necada.org>
2019-11-11 20:55:33 +0200

    Remove old Bessel transform calculations.
    These are absolutely unneeded for the article branch
    and only take space even in shallow copies.
    
    
    Former-commit-id: 58a5d5d29ee689d72d36de1bff11e80669261ad0

--- besseltransforms/vzor.m
+++
@@ -1,6 +0,0 @@
-$Assumptions = k >= 0 && k0 >= 0 && c >= 0 && n >= 0 ;
-Simplify[Refine[Integrate[(1 - Exp[-c x])^\[Kappa] (k0 x)^(-q) Exp[
-    I k0 x] x BesselJ[n, k x], {x, 
-       0, \[Infinity]}], {\[Kappa] == kk, q == qq, n == nn}]]
-Series[%, {k, \[Infinity], 10}]
-Quit[ ]
